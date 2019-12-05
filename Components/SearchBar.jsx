37c850836de03e98543f278c6b7f9344f671d003
erabin05 <e.rabin05@gmail.com>
2019-12-05 15:30:57 +0100

    can search for my account with button

--- Components/SearchBar.jsx
+++ Components/SearchBar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import axios from 'axios'
+
+const searchForAccount = (accountName) => {
+    axios.get(`https://api.github.com/users/${accountName}`)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err.message))
+}
+
+const onSubmit = (e, accountName) => {
+    e.preventDefault()
+    searchForAccount()
+}
 
 const SearchBar = () => {
     return (
         <form>
             <input type='text'/>
-            <input type='submit'/>
+            <input type='submit' onClick={(e) => onSubmit(e, 'erabin05')}/>
         </form>
     )
 }
