0c0bc206aab6c6c9bb7a37f082b83c49d0c9e840
erabin05 <e.rabin05@gmail.com>
2019-12-05 16:04:27 +0100

    detect if account found or not

--- Components/SearchBar.jsx
+++ Components/SearchBar.jsx
@@ -1,33 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const searchForAccount = (accountName, setAccount) => {
-    axios.get(`https://api.github.com/users/${accountName}`)
-        .then(res => setAccount(res.data))
-        .catch(err => console.log(err.message))
-}
-
-const onSubmit = (e, accountName, setAccount) => {
-    e.preventDefault()
-    searchForAccount(accountName, setAccount)
-}
-
-const onChange = (e, seIntputText) => {
-    seIntputText(e.target.value)
-}
-
 const SearchBar = () => {
     const [inputText, setInputText] = useState('')
     const [account, setAccount] = useState({})
-
-    useEffect(() => console.log(account), [account])
+    const [hasError, setHasError] = useState({status : false})
+    const [isSubmited, setIsSubmited] = useState(false)
 
     return (
         <form>
-            <input type='text' onChange={(e)=> onChange(e, setInputText)}/>
-            <input type='submit' onClick={(e) => onSubmit(e, inputText, setAccount)}/>
+            <input type='text' onChange={(e)=> onChange(e, setInputText, setIsSubmited)}/>
+            <input type='submit' onClick={(e) => onSubmit(e, inputText, setAccount, setHasError, setIsSubmited)}/>
+            { isSubmited 
+                && <p>{
+                    hasError.status 
+                    ?  hasError.message 
+                    : 'account found'
+                }</p>
+            }
         </form>
     )
+}
+
+const onChange = (e, seIntputText, setIsSubmited) => {
+    seIntputText(e.target.value)
+    setIsSubmited(false)
+}
+
+const onSubmit = (e, accountName, setAccount, setHasError, setIsSubmited) => {
+    e.preventDefault()
+    searchForAccount(accountName, setAccount, setHasError, setIsSubmited)
+}
+
+const searchForAccount = (accountName, setAccount, setHasError, setIsSubmited) => {
+    axios.get(`https://api.github.com/users/${accountName}`)
+        .then(res => {
+            setAccount(res.data)
+            setHasError({status : false})
+            setIsSubmited(true)
+        })
+        .catch(err => {
+            setHasError({
+                status : true,
+                message : err.message
+            })
+            setIsSubmited(true)
+        })
 }
 
 export default SearchBar
