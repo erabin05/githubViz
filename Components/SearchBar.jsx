b1b2f8d002ee051258fca8f988f708ead627327d
erabin05 <e.rabin05@gmail.com>
2019-12-05 15:46:24 +0100

    get account in state onSubmit

--- Components/SearchBar.jsx
+++ Components/SearchBar.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const searchForAccount = (accountName) => {
+const searchForAccount = (accountName, setAccount) => {
     axios.get(`https://api.github.com/users/${accountName}`)
-        .then(res => console.log(res.data))
+        .then(res => setAccount(res.data))
         .catch(err => console.log(err.message))
 }
 
-const onSubmit = (e, accountName) => {
+const onSubmit = (e, accountName, setAccount) => {
     e.preventDefault()
-    searchForAccount(accountName)
+    searchForAccount(accountName, setAccount)
 }
 
 const onChange = (e, seIntputText) => {
@@ -18,11 +18,14 @@ const onChange = (e, seIntputText) => {
 
 const SearchBar = () => {
     const [inputText, setInputText] = useState('')
+    const [account, setAccount] = useState({})
+
+    useEffect(() => console.log(account), [account])
 
     return (
         <form>
             <input type='text' onChange={(e)=> onChange(e, setInputText)}/>
-            <input type='submit' onClick={(e) => onSubmit(e, inputText)}/>
+            <input type='submit' onClick={(e) => onSubmit(e, inputText, setAccount)}/>
         </form>
     )
 }
