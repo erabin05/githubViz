f277f18e3f4128f14f872d7ad1e55bf2f475cab1
erabin05 <e.rabin05@gmail.com>
2019-12-05 15:21:52 +0100

    component searchBar

---
+++ Components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <form>
+            <input type='text'/>
+            <input type='submit'/>
+        </form>
+    )
+}
+
+export default SearchBar
